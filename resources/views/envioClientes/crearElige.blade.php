97bde170ac9d7d678b1e8299b2d2ad1427fb18cb
Gaston <gaston.marcilio@gmail.com>
2021-01-20 17:10:18 -0300

    menu
    menu elegir

--- resources/views/envioClientes/crearElige.blade.php
+++ resources/views/envioClientes/crearElige.blade.php
@@ -3,7 +3,30 @@
 @section('pageTitle', 'Elija accion')
 @section('content')
 
+<div class="row mt-5">
 
+    <div class="col-2"></div>
+    <div class="col-8">
+
+
+        <h2 align="center">Que desea hacer?</h2></br>
+    
+        <nav >
+        
+            <button type="button" class="btn btn-primary btn-lg btn-block" onclick="location.href='crear.php?cod=AS'">Directores</button>
+            <button type="button" class="btn btn-secondary btn-lg btn-block" onclick="location.href='crear.php?cod=GT'">Locales</button>
+            <button type="button" class="btn btn-primary btn-lg btn-block" onclick="location.href='crear.php?cod=E'">Empleados</button>
+            <button type="button" class="btn btn-secondary btn-lg btn-block" onclick="location.href='crear.php?cod=%'">Otros</button>
+            
+        
+        </nav>
+
+
+    </div>
+    <div class="col-2"></div>
+
+
+</div>
 
     
 @endsection
